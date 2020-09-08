38e4a6a431ee6c2a242fd62ceff62eeb8f9af060
root <root@opensheets.org>
2020-09-08 14:13:15 +0700

    initial commit

---
+++ src/index.js
@@ -0,0 +1,4 @@
+var renderer = require("@pankod/canvas2video").renderer;
+var encoder = require("@pankod/canvas2video").encoder;
+var Linear = require("gsap").Linear;
+var Bounce = require("gsap").Bounce;
